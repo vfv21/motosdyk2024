6bb5564bbb8e59032be073b5597ea9c01529b116
vfv <vero0621@hotmail.com>
2024-09-10 09:59:03 -0500

    Prubas funciones

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         \App\Models\User::factory(10)->create();
-        \App\Models\Servicio::factory(100)->create();
+        \App\Models\Servicio::factory(15)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
